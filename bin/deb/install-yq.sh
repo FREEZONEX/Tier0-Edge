187755eddfce27e32ceb7751944b3ab2ba0cde0c
zhuangmh <15167160211@163.com>
2025-11-11 09:53:08 +0800

    remove builds.yaml in script

--- bin/deb/install-yq.sh
+++ bin/deb/install-yq.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-if [ -f "$SCRIPT_DIR/../debs/yq_linux_amd64" ]; then
+if [ -f "$SCRIPT_DIR/../builds.yaml" ]; then
     cp $SCRIPT_DIR/../debs/yq_linux_amd64 /usr/local/bin/yq 
     chmod +x /usr/local/bin/yq 
 fi
