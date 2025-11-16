7fe6e6131dbb87a9d97e872b2d2ff6ecd4e2f6c3
zhuangmh <15167160211@163.com>
2025-11-16 11:58:40 +0800

    fix: ssl support portainer

--- bin/init/init-portainer.sh
+++ bin/init/init-portainer.sh
@@ -6,6 +6,7 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
 source $SCRIPT_DIR/../global/log.sh
 source $SCRIPT_DIR/../../.env
+source $SCRIPT_DIR/../../.env.tmp
 
 #info "start to init portainer OAuth ..."
 PORTAINER_JWT=`docker exec nodered curl -skX POST https://portainer:9443/api/auth      -H "Content-Type: application/json"      -d '{"username": "admin", "password": "adminpassword"}' | awk -F'"' '/jwt/ {print $4}'` && echo "Successfully got Portainer JWT"\
@@ -32,14 +33,14 @@ else
     -d "{
       \"authenticationMethod\": 3,
       \"oauthSettings\": {
-        \"AccessTokenURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN:$ENTRANCE_PORT/keycloak/home/auth/realms/supos/protocol/openid-connect/token\",
+        \"AccessTokenURI\": \"$BASE_URL/keycloak/home/auth/realms/supos/protocol/openid-connect/token\",
         \"AuthStyle\": 0,
-        \"AuthorizationURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN:$ENTRANCE_PORT/keycloak/home/auth/realms/supos/protocol/openid-connect/auth\",
+        \"AuthorizationURI\": \"$BASE_URL/keycloak/home/auth/realms/supos/protocol/openid-connect/auth\",
         \"ClientID\": \"$OAUTH_CLIENT_ID\",
         \"ClientSecret\": \"$OAUTH_CLIENT_SECRET\",
         \"OAuthAutoCreateUsers\": true,
-        \"RedirectURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN:$ENTRANCE_PORT/portainer/home/\",
-        \"ResourceURI\": \"$ENTRANCE_PROTOCOL://$ENTRANCE_DOMAIN:$ENTRANCE_PORT/keycloak/home/auth/realms/supos/protocol/openid-connect/userinfo\",
+        \"RedirectURI\": \"$BASE_URL/portainer/home/\",
+        \"ResourceURI\": \"$BASE_URL/keycloak/home/auth/realms/supos/protocol/openid-connect/userinfo\",
         \"SSO\": true,
         \"UserIdentifier\":\"preferred_username\",
         \"Scopes\":\"openid\"
